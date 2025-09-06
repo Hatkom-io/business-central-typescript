b7666e4c8cba211e8ad054b8298e4ac3769e4c19
Vladyslav Zavalykhatko <vladhatko@gmail.com>
2025-09-06 14:26:48 +0200

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { S3Service } from './s3-client.service'
-export { S3Module } from './s3-client.module'
+export { BusinessCentralApiModule } from './business-central-api.module'
+export { BusinessCentralApiService } from './business-central-api.service'
