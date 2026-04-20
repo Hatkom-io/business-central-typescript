b1eb4245c806dc85877893cbcae7fac05fcf8c2b
oleksii <do.gen.enterprise@gmail.com>
2026-04-20 17:17:05 +0300

    update param util to accept equal operation

--- src/util/param.util.ts
+++ src/util/param.util.ts
@@ -1,6 +1,6 @@
 export type Params<Entity> = {
   filter?: {
-    operation: 'startswith' | 'endswith' | 'contains'
+    operation: 'startswith' | 'endswith' | 'contains' | 'eq'
     field: Extract<keyof Entity, string> | string
     value: string
   }
@@ -9,6 +9,7 @@ export type Params<Entity> = {
     direction: 'asc' | 'desc'
   }
   top?: number
+  select?: Array<Extract<keyof Entity, string> | string>
 }
 
 export const formatParams = <Entity>(rawParams?: Params<Entity>) => {
@@ -27,7 +28,15 @@ export const formatParams = <Entity>(rawParams?: Params<Entity>) => {
   }
 
   if (rawParams.filter) {
-    query.$filter = `${rawParams.filter.operation}(${rawParams.filter.field},'${rawParams.filter.value}')`
+    const { operation, field, value } = rawParams.filter
+    query.$filter =
+      operation === 'eq'
+        ? `${field} eq '${value}'`
+        : `${operation}(${field},'${value}')`
+  }
+
+  if (rawParams.select?.length) {
+    query.$select = rawParams.select.join(',')
   }
 
   return query
